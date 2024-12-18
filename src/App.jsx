74c6dc11018decbd5971fea05312f5990b669586
YasDev <88037615+YasDev-coding@users.noreply.github.com>
2024-12-18 23:49:08 +0300

    Modified  vite template
    Added support for tailwincss and modified vite template to be coding ready

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-center font-semibold text-5xl text-indigo-600">
+        React + Vite + Tailwind Template
+      </h1>
+    </>
+  );
+}
